51565927f3c6488a1260ec179ed187e8f903bd1c
dieisongularte <dieissongularte@gmail.com>
2021-02-26 20:37:16 -0300

    add interface

---
+++ Classes/SerieRepositorio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using DIO.Series.Interfaces;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSeries = new List<Serie>();
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> Lista()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
